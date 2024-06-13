34f46d7717ef7e6d216259c5094da30df3322405
Daniel Douglas <dan.douglas.01@gmail.com>
2024-06-13 17:25:41 +0100

    Initial Commit

---
+++ PadMate/PadMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  PadMateApp.swift
+//  PadMate
+//
+//  Created by Daniel Douglas on 13/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct PadMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
